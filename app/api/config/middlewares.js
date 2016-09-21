e44821f45c455b34c4a1a72aa853e2836bd31b5d
Judith Medina <judith.medina@gigigo.com>
2016-09-21 12:30:27 +0200

    - Added giff and fixed the base layout.

--- app/api/config/middlewares.js
+++ app/api/config/middlewares.js
@@ -4,8 +4,8 @@ export default function(app){
     app.use(bodyParser.json({limit: '50mb'}));
     app.use(bodyParser.urlencoded({extended:true, limit: '50mb'}));
 
-    app.use(function(req, res, next) {
-      res.setHeader("Cache-Control", "max-age=31556926");
-      return next();
-    });
+    // app.use(function(req, res, next) {
+    //   res.setHeader("Cache-Control", "max-age=31556926");
+    //   return next();
+    // });
 }
